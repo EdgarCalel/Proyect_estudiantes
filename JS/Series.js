cc00cee9c2e8f405391a488da9472e89f6095d61
Carlos-Alfar0 <josecarlosalfaro070@gmail.com>
2021-12-06 11:14:11 -0600

    Proyecto Terminado

--- JS/Series.js
+++ JS/Series.js
@@ -6,3 +6,13 @@ window.addEventListener('load', function(){
 		dots: '.Indicadores',
 	});
 });
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 3.6,
+		slidesToScroll: 1,
+        draggable: true,
+		dots: '',
+
+	});
+});
